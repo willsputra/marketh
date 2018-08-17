10fd74fc6e2efa35db665f301b87a34fdd9dc5da
willsputra <suryadiputrawilliam@gmail.com>
2018-08-17 08:57:04 +0800

    add comments according to specs

--- contracts/Marketh.sol
+++ contracts/Marketh.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.22;
 
 import "zeppelin/contracts/math/SafeMath.sol";
 import "zeppelin/contracts/ownership/Ownable.sol";
 
+/** @title Marketh */
 contract Marketh is Ownable {
 
     using SafeMath for uint;
@@ -44,20 +45,14 @@ contract Marketh is Ownable {
 
     bool stopped; 
 
+    /** @dev Initialize CIRCUIT BREAKER */
     constructor() public {
-        // CIRCUIT BREAKER
         stopped = false;
     }
 
-    function toggleContractActive() onlyOwner public {
+    /** @dev CIRCUIT BREAKER: Allows owner to deactivate the contract */
+    function toggleContractActive() public onlyOwner {
         stopped = !stopped;
-    }
-
-
-    // MODIFIER storeOwner
-    modifier onlyStoreOwner(){
-        require(storeOwners[msg.sender] == true);
-        _;
     }
 
     modifier stopInEmergency() {
@@ -68,21 +63,33 @@ contract Marketh is Ownable {
         if(stopped) _;
     }
 
-    // FUNCTION addStoreOwner [OWNER] PRIVATE
+
+    modifier onlyStoreOwner(){
+        require(storeOwners[msg.sender] == true, "You are not a store owner.");
+        _;
+    }
+
+    /** @dev Adds an address as a Store Owner.
+      * @param _recipient Address of the Store Owner.
+      */       
     function addStoreOwner(address _recipient) public onlyOwner stopInEmergency{
         storeOwners[_recipient] = true;
         storeOwnersCount = storeOwnersCount.add(1);
     }
 
-    // FUNCTION removeStoreOwner [OWNER] PRIVATE
+    /** @dev Removes a Store Owner.
+      * @param _recipient Address of the Store Owner.
+      */    
     function removeStoreOwner(address _recipient) public onlyOwner stopInEmergency{
         storeOwners[_recipient] = false;
         storeOwnersCount = storeOwnersCount.sub(1);
     }
 
-    // FUNCTION viewStoreOwners [OWNER] PRIVATE
-
-    // FUNCTION addStore [STOREOWNER]
+    /** @dev Adds a Store.
+      * @param _imageUrl Image URL of the Store
+      * @param _title Title of the Store
+      * @param _description Description of the Store
+      */    
     function addStore(string _imageUrl, string _title, string _description) public onlyStoreOwner stopInEmergency {
         Store memory newStore = Store({
             imageUrl: _imageUrl,
@@ -99,14 +106,23 @@ contract Marketh is Ownable {
         storesCount = storesCount.add(1);
     }
 
-    // FUNCTION removeStore [STOREOWNER]
+    /** @dev Removes a Store.
+      * @param _storeId ID of the Store (index in stores array)
+      */ 
     function removeStore(uint _storeId) public onlyStoreOwner stopInEmergency {
         stores[_storeId].active = false;
     }
 
-    // FUNCTION addItem [STOREOWNER]
+    /** @dev Adds an Item.
+      * @param _storeId ID of the Store (index in stores array)
+      * @param _imageUrl Image URL of the Item
+      * @param _title Title of the Item
+      * @param _description Description of the Item
+      * @param _price Price of the Item
+      * @param _quantity Quantity of the Item
+      */    
     function addItem(uint _storeId, string _imageUrl, string _title, string _description, uint _price, uint _quantity) public onlyStoreOwner {
-        require(storeToStoreOwner[_storeId] == msg.sender);
+        require(storeToStoreOwner[_storeId] == msg.sender, "You are not the owner of this store.");
 
         Item memory newItem = Item({
             storeId: _storeId,
@@ -125,14 +141,23 @@ contract Marketh is Ownable {
         itemsCount = itemsCount.add(1);
      }
 
-    // FUNCTION removeItem [STOREOWNER]
+    /** @dev Removes an Item.
+      * @param _itemId ID of the Item (index in items array)
+      */ 
     function removeItem(uint _itemId) public onlyStoreOwner stopInEmergency {
         items[_itemId].active = false;
     }
 
-    // FUNCTION editItem [STOREOWNER]
+    /** @dev Edits an Item.
+      * @param _itemId ID of the Item (index in items array)
+      * @param _imageUrl Image URL of the Item (new)
+      * @param _title Title of the Item (new)
+      * @param _description Description of the Item (new)
+      * @param _price Price of the Item (new)
+      * @param _quantity Quantity of the Item (new)
+      */   
     function editItem(uint _itemId, string _imageUrl, string _title, string _description, uint _price, uint _quantity) public onlyStoreOwner {
-        require(storeToStoreOwner[items[_itemId].storeId] == msg.sender);
+        require(storeToStoreOwner[items[_itemId].storeId] == msg.sender, "You are not the store owner of this item.");
 
         items[_itemId].imageUrl = _imageUrl;
         items[_itemId].title = _title;
@@ -142,18 +167,20 @@ contract Marketh is Ownable {
     }
 
 
-    // FUNCTION buyItem [STOREOWNER | BUYER] [CANNOT BUY FROM OWN STORE]
+    /** @dev Buys an Item.
+      * @param _itemId ID of the Item (index in items array)
+      */   
     function buy(uint _itemId) public payable stopInEmergency {
-        require(msg.sender != storeToStoreOwner[items[_itemId].storeId]);
-        require(msg.value == items[_itemId].price);
-        require(items[_itemId].quantity > 0);
+        require(msg.sender != storeToStoreOwner[items[_itemId].storeId], "You are not able to buy your own item.");
+        require(msg.value == items[_itemId].price, "Value transferred is not the same as price.");
+        require(items[_itemId].quantity > 0, "This item is sold out.");
         
-        pendingWithdrawals[storeToStoreOwner[items[_itemId].storeId]] =  pendingWithdrawals[storeToStoreOwner[items[_itemId].storeId]].add(items[_itemId].price);
+        pendingWithdrawals[storeToStoreOwner[items[_itemId].storeId]] = pendingWithdrawals[storeToStoreOwner[items[_itemId].storeId]].add(items[_itemId].price);
         items[_itemId].quantity = items[_itemId].quantity.sub(1);
     }
     
-
-    // FUNCTION withdraw
+    /** @dev Withdraws the balance.
+      */   
     function withdraw() public {
 
         
